01a1e2aa14dc152a87729341b76c6cd24854864b
yubingcen <644268318@qq.com>
2023-05-08 18:11:57 +0800

    feat: 修改分包使用插件的配置

--- subpackages/uni-app/src/kivicube/pages/index.vue
+++ subpackages/uni-app/src/kivicube/pages/index.vue
@@ -18,7 +18,14 @@
 </template>
 
 <script>
-const { clearSceneCache } = requirePlugin("kivicube");
+const { clearSceneCache, setPackageRootPath, setOptions } = requirePlugin("kivicube");
+
+// 使用license可以去除水印
+setOptions({
+  license: "OA7EIJ7JB4JmAZ5CiceSt0ScZael9lB0GJTb+5YJ5B6l8idXnc3eJ5/8wlH44ZPycUu1anh7DuOremv6GKmChel5PT4WcQNqny5/vpWy4Zr7Y119sdRK7bGEpBVx6b9IpO2/otJ7e7qQza9ESKBR+0i2EaYxd9khEdWE/ofgsONfEDW+mWqT26i598ev1zbhO8lwGYbUcqhL0UIrTjdLpfG0sBp2a4WqNc3YOnoh0e5EnzRn3h1AXVHpjutf86vwuDgJKq/MTVBZ7Y3dP7K7Jbye9whYL2nrgRQkvw38nsuo/FspqweCujz7MzP1wPpYZLms2KtgGdjzSfuTbWxThQ=="
+});
+
+setPackageRootPath("kivicube"); // 当分包使用此插件时，必须在使用组件前，调用此方法，设置分包路径。
 
 export default {
   methods: {
